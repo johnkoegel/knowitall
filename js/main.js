eda02df94a14dd5c930217248014aa8ed87e452d
johnkoegel <john.m.koegel@gmail.com>
2014-03-29 15:03:40 -0500

    part 5 - created html template for easier dynamic changes to single page application, insertion into single DOM

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
-    renderHomeView: function () {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -32,8 +8,22 @@ var app = {
         }
     },
 
+    findByName: function () {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function (employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
+    renderHomeView: function () {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     initialize: function () {
         var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function () {
             self.renderHomeView();
         });
